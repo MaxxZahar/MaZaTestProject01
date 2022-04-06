75446a3c04c4080028437dc6a10bc7320b1d65d4
Maksim Zakharenkov <maxxzahar@rambler.ru>
2022-04-07 02:17:40 +0300

    simple menu

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -1,1 +1,19 @@
 from garpixcms.settings import *  # noqa
+
+INSTALLED_APPS += [  # noqa
+    'home',
+]
+
+MENU_TYPE_HEADER_MENU = 'header_menu'
+MENU_TYPE_FOOTER_MENU = 'footer_menu'
+
+MENU_TYPES = {
+    MENU_TYPE_HEADER_MENU: {
+        'title': 'Верхнее меню',
+    },
+    MENU_TYPE_FOOTER_MENU: {
+        'title': 'Нижнее меню',
+    }
+}
+
+CHOICE_MENU_TYPES = [(k, v['title']) for k, v in MENU_TYPES.items()]
