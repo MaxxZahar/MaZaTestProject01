2700ee1b4e7c182b73e1d775a5217c94927c90b9
Maksim Zakharenkov <maxxzahar@rambler.ru>
2022-04-15 09:49:32 +0300

    hotels ordering back

--- backend/hotels/models/hotel_list_page.py
+++ backend/hotels/models/hotel_list_page.py
@@ -3,14 +3,20 @@ from garpix_page.models import BaseListPage
 
 
 class HotelListPage(BaseListPage):
-    paginate_by = 25
+    paginate_by = 2
     template = 'pages/hotel_list.html'
 
     def get_context(self, request=None, *args, **kwargs):
         from .hotel import Hotel
         from ..serializers import HotelSerializer
         context = super().get_context(request, *args, **kwargs)
-        hotels = HotelSerializer(Hotel.objects.all(), many=True).data
+        queryset = Hotel.objects.all()
+        par = request.GET.get('ordering', False)
+        ordering_set = {'price', 'type', 'rating'}
+        ordering_set_reverse = {'-' + item for item in ordering_set}
+        if par in ordering_set or par in ordering_set_reverse:
+            queryset = queryset.order_by(par)
+        hotels = HotelSerializer(queryset, many=True).data
         context.update({
             'hotels': hotels
         })
